3ca2a5ec5dae1356f10047c1ecfb0cc93ea2cc5e
diakvlv <112659334+diakvlv@users.noreply.github.com>
2023-06-19 00:08:34 +0200

    MERGE

--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -3,6 +3,11 @@ interface Data {
   elements: { title: string, image: string }[]
 }
 
+import LinkWeather from './components/linkWeather.vue';
+import RightHead from './components/rightHead.vue';
+import PreviewTemp from './components/previewTemp.vue';
+import MiddleInf from './components/middleInf.vue';
+
 const data: Data = {
   elements: [
    { title: 'Logo', image: '../public/cuacane-logo.png' },
@@ -57,6 +62,53 @@ const data: Data = {
 
     </div>
     <div class="middleContainer">
+
+            <div class="search">
+         <div class="searchBar">
+          Willkomen
+         </div>
+         <div class="lupa">
+          <a href=""><img src="https://cdn-icons-png.flaticon.com/512/1167/1167092.png" alt=""></a> 
+        </div>
+        <div class="lupa">
+          <a href=""><img src="https://cdn-icons-png.flaticon.com/512/40/40031.png" alt=""></a>  
+        </div>
+      </div>
+      <div class="middle1">
+        <div class="lev">
+         <div class="blank">
+          <div class="text-left">
+            Gotham
+          </div>
+          <div class="text-left1">
+            1016hPa
+            </div>
+          </div>
+          <div class="blank">
+          <div class="tempBig">
+            14°
+          </div>
+          <div class="text-middle">
+           Polojasno
+            </div>
+        </div>
+          <div class="blank">
+          <div class="text-right">
+            Monday
+          </div>
+          <div class="text-right2">
+           20kph
+            </div>
+          </div>
+        </div>
+     
+        <div class="obr">
+          <img
+            src="../src/assets/temp.PNG">
+        </div>
+      </div>
+
+
       
       <div class="window-section">
   <div class="window">
@@ -95,7 +147,52 @@ const data: Data = {
 
     </div>
     <div class="rightContainer">
-      PRAVÝ
+        <div class="right">
+      <RightHead head1="<" head2="Head" head3=">" />
+        <br><br><br>
+      <div class="prev">
+        <PreviewTemp temparature="17" city="Prag"
+          image1="https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Eo_circle_light-blue_blank.svg/2048px-Eo_circle_light-blue_blank.svg.png"
+          image2="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Yellow_icon.svg/2048px-Yellow_icon.svg.png" />
+        <PreviewTemp temparature="14" city="Prag"
+          image1="https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Eo_circle_light-blue_blank.svg/2048px-Eo_circle_light-blue_blank.svg.png"
+          image2="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Yellow_icon.svg/2048px-Yellow_icon.svg.png" />
+        <PreviewTemp temparature="13" city="Prag"
+          image1="https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Eo_circle_light-blue_blank.svg/2048px-Eo_circle_light-blue_blank.svg.png"
+          image2="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Yellow_icon.svg/2048px-Yellow_icon.svg.png" />
+        <PreviewTemp temparature="13" city="Prag"
+          image1="https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Eo_circle_light-blue_blank.svg/2048px-Eo_circle_light-blue_blank.svg.png"
+          image2="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Yellow_icon.svg/2048px-Yellow_icon.svg.png" />
+      </div>
+<br><br><br>
+      <LinkWeather name="Praha" temparature="21"
+        image1="https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Eo_circle_light-blue_blank.svg/2048px-Eo_circle_light-blue_blank.svg.png"
+        image2="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Yellow_icon.svg/2048px-Yellow_icon.svg.png" />
+      <LinkWeather name="Brno" temparature="13"
+        image1="https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Eo_circle_light-blue_blank.svg/2048px-Eo_circle_light-blue_blank.svg.png"
+        image2="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Yellow_icon.svg/2048px-Yellow_icon.svg.png" />
+      <LinkWeather name="Ostrava" temparature="18"
+        image1="https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Eo_circle_light-blue_blank.svg/2048px-Eo_circle_light-blue_blank.svg.png"
+        image2="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Yellow_icon.svg/2048px-Yellow_icon.svg.png" />
+      <LinkWeather name="Olomouc" temparature="21"
+        image1="https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Eo_circle_light-blue_blank.svg/2048px-Eo_circle_light-blue_blank.svg.png"
+        image2="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Yellow_icon.svg/2048px-Yellow_icon.svg.png" />
+      <LinkWeather name="Liberec" temparature="14"
+        image1="https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Eo_circle_light-blue_blank.svg/2048px-Eo_circle_light-blue_blank.svg.png"
+        image2="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Yellow_icon.svg/2048px-Yellow_icon.svg.png" />
+      <LinkWeather name="Karlovy Vary" temparature="21"
+        image1="https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Eo_circle_light-blue_blank.svg/2048px-Eo_circle_light-blue_blank.svg.png"
+        image2="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Yellow_icon.svg/2048px-Yellow_icon.svg.png" />
+      <LinkWeather name="J. Hradec" temparature="13"
+        image1="https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Eo_circle_light-blue_blank.svg/2048px-Eo_circle_light-blue_blank.svg.png"
+        image2="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Yellow_icon.svg/2048px-Yellow_icon.svg.png" />
+      <LinkWeather name="Jihlava" temparature="12"
+        image1="https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Eo_circle_light-blue_blank.svg/2048px-Eo_circle_light-blue_blank.svg.png"
+        image2="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Yellow_icon.svg/2048px-Yellow_icon.svg.png" />
+      <LinkWeather name="Chomutov" temparature="21"
+        image1="https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Eo_circle_light-blue_blank.svg/2048px-Eo_circle_light-blue_blank.svg.png"
+        image2="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Yellow_icon.svg/2048px-Yellow_icon.svg.png" />
+      </div>
     </div>
   </div>
 </template>
@@ -186,7 +283,6 @@ const data: Data = {
 
 .menu a.active {
   font-weight: bold;
-  border-right: 1px solid black;
 }
 
 .menu img {
@@ -262,6 +358,123 @@ const data: Data = {
   width: 100%;
   height: 100%;
   object-fit: cover;
+}
+
+
+
+
+.right{
+  background-color: white;
+}
+
+.middle1 img{
+  border-radius: 20px;
+}
+.lupa{
+  margin: 15px;
+}
+.blank{
+  background-color:  #303c54;
+}
+.searchBar{
+ align-items: center;
+  margin: 10px;
+  background-color: white;
+  height: 30px;
+  padding-top: 0.5%;
+  border-radius: 20px;
+  padding-left: 40px;
+  width: 80%;
+}
+.search{
+ height: 60px;
+  margin: 40px;
+  padding-top: 10px;
+  border-radius: 25px;
+  display: flex;
+}
+.text-middle{
+  background-color: #303c54;
+  text-align: center;
+  padding-left: 10px;
+}
+.tempBig {
+  background-color: #303c54;
+  padding-top: 40%;
+  width: 120px;
+  text-align: center;
+  font-size: 80px;
+  padding-left: 10px;
+}
+.lupa img{
+  width: 30px;
+}
+.lev {
+  background-color: #303c54;
+  width: 50%;
+  padding: 20px;
+  flex-direction: row;
+  display: flex;
+}
+
+.text-left {
+  background-color: #303c54;
+  margin-left: auto;
+}
+.text-right2{
+  padding-top: 280px;
+  background-color: #303c54;
+  text-align: center;
+  margin-left: auto;
+}
+.text-left1{
+  background-color: #303c54;
+  padding-top: 280px;
+  text-align: center;
+  margin-left: auto;
+}
+.text-right {
+  background-color: #303c54;
+ margin-left: auto;
+}
+
+.middle1 {
+  display: flex;
+  padding: 50px;
+  margin: 40px;
+  height: 340px;
+  background-color: #303c54;
+  border-radius: 20px;
+  color:white;
+  min-height: 450px;
+  min-width: 500px;
+}
+
+.obr {
+  height: 350px;
+  border-radius: 40px;
+}
+
+.obr img {
+  object-fit: cover;
+  width: 100%;
+  height: 100%;
+}
+
+.prev {
+  display: flex;
+  gap: 20px;
+  justify-content: space-between;
+  padding: 0 20px;
+  background-color: white;
+}
+
+.mod {
+  flex-direction: column;
+}
+
+.RightHead {
+  height: 100px;
 }
 
 </style>
